a41d1243479f1342fdaf8458ca22c5402257f3d6
Benjamin Saenz <bsaenz454@gmail.com>
2024-09-19 14:19:55 +0000

    changed workspace structure

--- src/rmd_motor_hardware/build/rmd_motor_hardware/CMakeFiles/rmd_motor_hardware_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rmd_motor_hardware_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rmd_motor_hardware_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
